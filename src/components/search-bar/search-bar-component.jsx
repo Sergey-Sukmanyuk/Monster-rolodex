d9eecad289a2975b5652bceeb47ec535ac97c66d
Serhii Sukmaniuk <ssukmanyuk@gmail.com>
2022-11-17 16:35:15 +0200

    changed class components on functionaal components

--- src/components/search-bar/search-bar-component.jsx
+++ src/components/search-bar/search-bar-component.jsx
@@ -1,20 +1,15 @@
-import { Component } from "react";
-
 import './search-bar-styles.css'
 
-class SearchBar extends Component {
-  render() {
-    const { searchHeandler, className, placeholder } = this.props;
-    return (
-      <div>
-        <input 
-        className={`search-box ${className}`}
-        placeholder={placeholder} 
-        type="search" 
-        onChange={searchHeandler} />
-      </div>
-    );
-  }
+const SearchBar = ({ searchHeandler, className, placeholder }) => {
+  return (
+    <div>
+      <input 
+      className={`search-box ${className}`}
+      placeholder={placeholder} 
+      type="search" 
+      onChange={searchHeandler} />
+    </div>
+  );
 }
 
 export default SearchBar;
